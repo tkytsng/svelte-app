51f1832cef2012a48c29add91c7d5bb72212f535
Kor <chanchara@gmail.com>
2019-12-04 15:22:21 +0900

    update

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,5 @@
 <script lang="ts">
   import GetMotsu from "./components/GetMotsu.svelte";
-
-  let data;
-  function test(event) {
-    console.log(event.detail.data);
-    data = event.detail.data;
-  }
 </script>
 
 <GetMotsu />
